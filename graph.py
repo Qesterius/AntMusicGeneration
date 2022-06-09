3eceb2fd84c164817d03b5cec3edf31330cfa6a5
Luki <hexeyxd@gmail.com>
2022-06-09 14:54:42 +0200

    sentino midi added

--- graph.py
+++ graph.py
@@ -1,6 +1,9 @@
+
+
 class Nutka:
     #wysokosc dzwieku
     #dlugosc dzwieku nw jak robimy jesazce z taktowaniem -> to takie co jest 4/4 na przyklad
+
 
     def __init__(self):
 
@@ -8,3 +11,10 @@ class Nutka:
 class Graph:
     #nodes
     #adjencymatrix
+
+    def createMusic(self, scale, bpm, metryka ):
+        #scale to moze byc rownie dobrze tutaj maska po prostu jakie wierzcholki bedziemy uzywac (ostatecznie chcemy, zeby wierzcholkow bylo przynajmniej z jakis 2 utworow o roznych skalach)
+        #bpm to w sumie wyjebane, bo to kwestia czy wszystko bedzie szybciej czy wolniej, mozna ustawic jako stale nawet
+        #metryke mozemy ustalic 4/4 bo jest podstawowwe, ale w sumie trzeba do tego przysiasc https://pl.wikipedia.org/wiki/Takt_(muzyka) / https://pl.wikipedia.org/wiki/Metrum_(muzyka)
+
+        il_jedn_metr_na_takt,jednostka_metryczna = metryka # to oznacza tyle, ze na jeden takt w utworze przypada ilestam cwiercnut/szesnastek itd.
