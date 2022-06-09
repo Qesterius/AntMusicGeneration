d4058b7e368e4b8412479bf96f0077554c5b24d3
Luki <hexeyxd@gmail.com>
2022-06-09 08:46:44 +0200

    todoos

---
+++ main.py
@@ -0,0 +1,11 @@
+#TODO: obtain midi Sentino Midas and maybe some others
+#TODO: MIDI library
+#mido https://mido.readthedocs.io/en/latest/message_types.html
+import mido
+#TODO: MIDI instruments/samples
+#z teoria muzyki too jakos wspolgra, ale nw czy bedzie warto ogarniac https://pythonlang.dev/repo/rainbow-dreamer-musicpy/
+#TODO: graph of midi events
+#TODO: ant traversing implementation
+#TODO: playing music
+#TODO: saving music
+
