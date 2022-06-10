5032551e3ad389f5721890f737b6244dac888b3e
Mateusz Wiecek <mateusz.wiecek12345@gmail.com>
2022-06-10 11:19:34 +0200

    working

--- graph.py
+++ graph.py
@@ -6,11 +6,10 @@ class Note:
     # wysokosc dzwieku
     # dlugosc dzwieku nw jak robimy jesazce z taktowaniem -> to takie co jest 4/4 na przyklad
 
-    def __init__(self, time, note,velocity):
+    def __init__(self, time, note, velocity):
         self.time = time
         self.note = note
         self.velocity = velocity
-
 
     def create_message(self):
         msg = mido.Message('note_on', channel=0, note=self.note, velocity=64, time=0)
@@ -23,38 +22,45 @@ class Note:
 class Graph:
 
     def initialize_pheromones(self, route):
-        pheromones = [[0 for _ in range(len(self.notes))] for _ in range(len(self.notes))]
-        for move in enumerate(route):
-            pass
+        route = list(map(lambda x: x[1], route))
+        # print(max(route))
+        time = 10
+        pheromones = [[0 for _ in range(max(route)+1)] for _ in range(max(route)+1)]
+        ants = []
+        for idx, move in enumerate(route):
+            if idx != 0:
+                pheromones[route[idx-1]][move] += self.pheromone_increase
+        return pheromones
 
-    def __init__(self, notes, ants_num, pheromone_increase, pheromone_decrease, ants_starting_route):
-        self.notes = notes
-        self.ants = [randint(min(notes), max(notes) + 1) for _ in range(ants_num)]
+    def __init__(self, notes, ants, pheromone_increase, pheromone_decrease, ants_starting_route):
+        self.notes = {note.note: note for note in notes}
+        self.ants = ants
         self.last_moves = []
         self.pheromone_increase = pheromone_increase
         self.pheromone_decrease = pheromone_decrease
         self.music = []
 
-        # self.pheromones =
+        self.pheromones = self.initialize_pheromones(ants_starting_route)
+        # print(len(self.pheromones))
 
     # nodes
     # adjencymatrix
     def move_ants(self):
         n = len(self.notes)
         last_moves = []
-        for ant in self.ants:
+        for idx, ant in enumerate(self.ants):
             _sum = 0
-            weights = [0 for _ in range(n)]
+            weights = [0 for _ in range(max(self.notes.keys()) + 1)]
+            for key in self.notes:
+                _sum += self.pheromones[ant][key] / (abs(key - ant) + 1)
 
-            for i in range(n):
-                _sum += self.pheromones[ant][i] / abs(self.notes[i].note - self.notes[ant].note)
-
-            for i in range(n):
-                weights[i] = self.pheromones[ant][i] / abs(self.notes[i].note - self.notes[ant].note) / _sum
-            move = choices([i for i in range(n)], weights=weights)
+            for key in self.notes:
+                weights[key] = self.pheromones[ant][key] / (abs(key - ant) + 1) / _sum
+            move = choices([i for i in range(max(self.notes.keys())+1)], weights=weights)
+            move = move[0]
             # adding moves
             self.music.append((self.notes[move].note, self.notes[move].time))
-            self.ants[ant] = move
+            self.ants[idx] = move
             self.last_moves.append((ant, move))
 
         for move_from, move_to in self.last_moves:
@@ -64,6 +70,7 @@ class Graph:
             for j in range(n):
                 self.pheromones[i][j] *= self.pheromones[i][j] * (1 - self.pheromone_decrease)
 
+
     def create_music(self, scale, bpm, metryka, length):
         # scale to moze byc rownie dobrze tutaj maska po prostu jakie wierzcholki bedziemy uzywac (ostatecznie chcemy, zeby wierzcholkow bylo przynajmniej z jakis 2 utworow o roznych skalach)
         # bpm to w sumie wyjebane, bo to kwestia czy wszystko bedzie szybciej czy wolniej, mozna ustawic jako stale nawet
@@ -72,3 +79,4 @@ class Graph:
         il_jedn_metr_na_takt, jednostka_metryczna = metryka  # to oznacza tyle, ze na jeden takt w utworze przypada ilestam cwiercnut/szesnastek itd.
         for i in range(length):
             self.move_ants()
+        return self.music
--- main.py
+++ main.py
@@ -1,98 +1,123 @@
-#TODO: obtain midi Sentino Midas and maybe some others
-#TODO: MIDI library
-#mido https://mido.readthedocs.io/en/latest/message_types.html
+# TODO: obtain midi Sentino Midas and maybe some others
+# TODO: MIDI library
+# mido https://mido.readthedocs.io/en/latest/message_types.html
 import mido
 from queue import PriorityQueue
-#TODO: MIDI instruments/samples
-#z teoria muzyki too jakos wspolgra, ale nw czy bedzie warto ogarniac https://pythonlang.dev/repo/rainbow-dreamer-musicpy/
-#TODO: graph of midi events
-#TODO: ant traversing implementation
-#TODO: playing music
-#TODO: saving music
+
+# TODO: MIDI instruments/samples
+# z teoria muzyki too jakos wspolgra, ale nw czy bedzie warto ogarniac https://pythonlang.dev/repo/rainbow-dreamer-musicpy/
+# TODO: graph of midi events
+# TODO: ant traversing implementation
+# TODO: playing music
+# TODO: saving music
 
 
-#1) mido trzeba zaladowac ta biblioteka plik midi (np ten co jest na gicie) i zobaczyc jak on tam wyglada
+# 1) mido trzeba zaladowac ta biblioteka plik midi (np ten co jest na gicie) i zobaczyc jak on tam wyglada
 # w sensie jak te kolejne zdzarzenia midi wystepuja i czy tam sie gdzies ustawia jaki instrument gra
 # poza sama osia czasu jakby, czy jak
 
-#2) jakos na podstawie tego uzupelnic nasza klase nutek i nastepnie Grafu muzyki
-#3) zagrac na naszym grafie sentino
- #3.2) ogarnac co to jest za wyrazenie w tamtej pracy ktorym mrowka decyduje gdzie pojsc krawedziami
-#4) ustawic przy tym zagraniu poczatkowe wartosci na krawedziach (?)
+# 2) jakos na podstawie tego uzupelnic nasza klase nutek i nastepnie Grafu muzyki
+# 3) zagrac na naszym grafie sentino
+# 3.2) ogarnac co to jest za wyrazenie w tamtej pracy ktorym mrowka decyduje gdzie pojsc krawedziami
+# 4) ustawic przy tym zagraniu poczatkowe wartosci na krawedziach (?)
 # jakos tak zrozumialem z tej pracy ale nie jestem pewny
 
-#5) puscic mruweczki po grafie, zeby cos naklikaly i pozapisywaly do pliku midi
-#6) odtworzyc plik wynikowy uzywajac playmidi
+# 5) puscic mruweczki po grafie, zeby cos naklikaly i pozapisywaly do pliku midi
+# 6) odtworzyc plik wynikowy uzywajac playmidi
+
+from graph import Graph
+import random
 
 sentino = mido.MidiFile('midi/sentino.mid')
 out = mido.MidiFile()
-print("ticks",sentino.ticks_per_beat)
+print("ticks", sentino.ticks_per_beat)
+
+
 def printTrack(track):
-        for msg in track:
-            print(msg)
+    for msg in track:
+        print(msg)
+
 
 def process(track):
     notes_non_dup = []
-    velocity_arr=[]
-    timeline=[]
-    cur_time=0
-    for i,msg in enumerate(track):
-        cur_time+=msg.time
+    velocity_arr = []
+    timeline = []
+    route = []
+    cur_time = 0
+    for i, msg in enumerate(track):
+        cur_time += msg.time
         if msg.type == 'note_on':
             note = msg.note
-            time =0
-            j=i+1
+            time = 0
+            j = i + 1
             while track[j].type != 'note_off' or track[j].note != note:
-                #if(note == 68):
-                    #print(track[j].note)
-                time+= track[j].time
-                j+=1
-            #print()
+                # if(note == 68):
+                # print(track[j].note)
+                time += track[j].time
+                j += 1
+            # print()
             time += track[j].time
-            index=0
-            if (note,time) in notes_non_dup:
-                index = notes_non_dup.index((note,time))
+            index = 0
+            if (note, time) in notes_non_dup:
+                index = notes_non_dup.index((note, time))
 
             else:
                 index = len(notes_non_dup)
-                notes_non_dup.append((note,time))
+                notes_non_dup.append((note, time))
                 velocity_arr.append(msg.velocity)
-            timeline.append((cur_time,index))
+            timeline.append((cur_time, index))
+            route.append((cur_time, note))
 
-    return notes_non_dup,velocity_arr,timeline
+    return notes_non_dup, velocity_arr, timeline, route
+
+
 from graph import Note
 
-def createNotes(keys,velocity_arr):
-    Notes=[]
-    for i,key in enumerate(keys):
-        note,time = key
-        Notes.append(Note(time,note,velocity_arr[i]))
+
+def createNotes(keys, velocity_arr):
+    Notes = []
+    for i, key in enumerate(keys):
+        note, time = key
+        Notes.append(Note(time, note, velocity_arr[i]))
 
     return Notes
 
-def recreateMidifromGraphPath(path,keys):
+
+def create_notes_without_time(keys, velocity_arr):
+    Notes = []
+    keys = map(lambda x: x[0], keys)
+    keys = list(set(keys))
+    # print(keys)
+    for i, key in enumerate(keys):
+        note = key
+        Notes.append(Note(4, note, velocity_arr[i]))
+    return Notes
+
+
+def recreateMidifromGraphPath(path, keys):
     file = mido.MidiFile()
     file.ticks_per_beat = sentino.ticks_per_beat
     outTrack = mido.MidiTrack()
     file.tracks.append(outTrack)
     queue = PriorityQueue()
     for p in path:
-        queue.put( (p[0],p[1], 1))
+        queue.put((p[0], p[1], 1))
 
-    last_time=0
+    last_time = 0
     while not queue.empty():
         top = queue.get()
         action = 'note_on' if top[2] == 1 else 'note_off'
         timeline_time = top[0]
         note_ind = top[1]
-        #print(note_ind)
+        # print(note_ind)
         if action == 'note_on':
-            queue.put((timeline_time+keys[note_ind].time,top[1],0))
+            queue.put((timeline_time + keys[note_ind].time, top[1], 0))
 
-        outTrack.append(mido.Message(action,note=keys[note_ind].note,velocity=keys[note_ind].velocity,time=timeline_time-last_time))
+        outTrack.append(mido.Message(action, note=keys[note_ind].note, velocity=keys[note_ind].velocity,
+                                     time=timeline_time - last_time))
         last_time = timeline_time
 
-    #printTrack(outTrack)
+    # printTrack(outTrack)
 
     print("ticks", file.ticks_per_beat)
     global out
@@ -100,21 +125,32 @@ def recreateMidifromGraphPath(path,keys):
     file.save("midi/out.mid")
 
 
+notess, velocity_arrr, timeli, route = process(sentino.tracks[0])
+# print("notes:", notess)
+# print("timeli:", timeli)
+# print("route", route)
+not_without_time = create_notes_without_time(notess, velocity_arrr)
+ants_num = 5
+ants = random.choices(list(map(lambda x: x[0], notess)), k=ants_num)
+print(ants)
 
-notess,velocity_arrr,timeli = process(sentino.tracks[0])
-print("notes:",notess)
-print("timeli:",timeli)
+notess = createNotes(notess, velocity_arrr)
 
-notess = createNotes(notess,velocity_arrr)
-recreateMidifromGraphPath(timeli,notess)
+graph = Graph(notes=not_without_time, ants=ants, pheromone_increase=1, pheromone_decrease=0.01,
+              ants_starting_route=route)
+music = graph.create_music(scale=10, bpm=190, metryka=(1, 1), length=1000)
+print(music)
+
+recreateMidifromGraphPath(timeli, notess)
 
 import sys
-original_stdout = sys.stdout # Save a reference to the original standard output
+
+original_stdout = sys.stdout  # Save a reference to the original standard output
 
 with open('LOG.txt', 'w') as f:
-    sys.stdout = f # Change the standard output to the file we created.
+    sys.stdout = f  # Change the standard output to the file we created.
     printTrack(sentino.tracks[0])
     print(" ")
     printTrack(out.tracks[0])
 
-    sys.stdout = original_stdout # Reset the standard output to its original value
+    sys.stdout = original_stdout  # Reset the standard output to its original value
